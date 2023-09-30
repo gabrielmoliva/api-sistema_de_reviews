b855b67787f410d8bc976421b0f9f2e44eccf438
gabrielmoliva <moliva.gabriel@gmail.com>
2023-09-30 19:02:29 -0300

    routes criados para api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/usuario', [UsuarioController::class, 'index']);
+Route::post('/usuario', [UsuarioController::class, 'store']);
+Route::put('/usuario', [UsuarioController::class, 'update']);
+Route::delete('/usuario', [UsuarioController::class, 'destroy']);
+
+Route::get('/review', [ReviewController::class, 'index']);
+Route::post('/review', [ReviewController::class, 'store']);
+Route::put('/review', [ReviewController::class, 'update']);
+Route::delete('/review', [ReviewController::class, 'destroy']);
+
+Route::get('/comenta', [ComentaController::class, 'index']);
+Route::post('/comenta', [ComentaController::class, 'store']);
+Route::put('/comenta', [ComentaController::class, 'update']);
+Route::delete('/comenta', [ComentaController::class, 'destroy']);
